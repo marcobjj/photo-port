11def79ad5d0d909e273cc92d8c0aa9672ef1b35
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-12-07 13:54:39 -0800

    initial

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import About from './components/about';
+import Nav from './components/nav';
+
+function App() {
+
+  return (
+    
+      <div>
+        <Nav></Nav>
+        <main>
+          <About></About>
+        </main>
+      </div>
+    
+  );
+}
+
+export default App;
+
+
+// import logo from './logo.svg';
+// import './App.css';
+// import About from './components/about';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+//}
+
